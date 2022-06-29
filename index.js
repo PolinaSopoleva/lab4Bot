9ab8ff47aaee8e0d5ec789baec4ff2371239a709
PolinaSopoleva <71347248+PolinaSopoleva@users.noreply.github.com>
2022-06-29 16:38:54 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import './env.js';
 import { Telegraf } from 'telegraf'
 
 const bot = new Telegraf("5493678079:AAHU7jN15-m7iAoMe_E0yVcQyv_v3uxpLAY")
-bot.start((ctx) => ctx.reply('Welcome'))
+bot.start((ctx) => ctx.reply('поставте пожалуйста ешку'))
 bot.help((ctx) => ctx.reply('Send me a sticker'))
 bot.on('sticker', (ctx) => ctx.reply('👍'))
-bot.hears('hi', (ctx) => ctx.reply('Не пиши сюда'))
+bot.hears('hi', (ctx) => ctx.reply('поставте пожалуйста ешку'))
 bot.launch()
 
 // Enable graceful stop
